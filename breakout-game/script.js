2b91ae0c2f36a7f5596dbc277f0f6c823424bc49
areebaqamar021 <areebaqamar021@gmail.com>
2021-03-27 15:10:53 +0500

    breakout game

---
+++ breakout-game/script.js
@@ -0,0 +1,62 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+const delay = 500;
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+    visible: true
+};
+
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+    visible: true
+}
+
+const breakInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++){
+    bricks[i] = [];
+    for (let j = 0; j < brickColumnCount; j++){
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = {x, y, ...brickInfo };
+    }
+}
+
+function drawBall(){
+    ctx.beingPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = ball.visible ? '#0095dd' : 'transparent';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle(){
+    
+}
