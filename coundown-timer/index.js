92635c51203438ee7ae07714969e9d3a8bb12932
areebaqamar021 <areebaqamar021@gmail.com>
2021-06-12 12:28:03 +0500

    done

---
+++ coundown-timer/index.js
@@ -0,0 +1,23 @@
+const months = [
+    'January',
+    'Febraury',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+];
