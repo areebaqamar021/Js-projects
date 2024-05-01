a9fe6d34e1a6d3765bd349a359c4fb5ebe06919e
areebaqamar021 <areebaqamar021@gmail.com>
2024-05-01 16:51:59 +0500

    okk

--- dumy-text-generator/app.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('#form');
-const copy = document.querySelector('.copy');
-
-form.addEventListener('', (e) => {
-    e.preventDefault()
-    const size = parseInt('form.input.value');
-})
