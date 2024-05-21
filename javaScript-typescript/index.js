3482eac46472ec516fe11003e6a84a4ad13ffc92
areebaqamar021 <areebaqamar021@gmail.com>
2024-05-21 20:28:14 +0500

    basic JavaScript problems, including printing even numbers and a multiplication table.

--- javaScript-typescript/index.js
+++ javaScript-typescript/index.js
@@ -1,35 +1,20 @@
-// console.log('areeba');
+// ##basic javascript problems
 
-// let firstName = 'john'
-// let lastName = "doe"
-// console.log(`my name is ${firstName} ${lastName}`);
+// Problem 1 : Print all even numbers from 0 – 10
 
-// console.log(firstName.slice(0,2));
+for (let i=0; i<10; i++){
+    if(i%2 ===0){
+        console.log(i);
+    }
+}
 
-// console.log(lastName.includes('e'));
+// Problem 2 : Print a table containing multiplication tables
 
-// let arrNames = ['abc', 'def', 'ghi'];
-// console.log(arrNames)
-// for(let i=0; i<arrNames.length; i++){
-//     console.log(arrNames[i])
-// }
+console.log('multiplication of tables:');
 
-// let names = function(name='Areeba', fatherName= 'Qamar'){
-//     console.log(`My name is ${name} and my father name is ${fatherName}`)
-// }
-// names()
-
-// const size = 5; // Change this value to increase or decrease the size of the diamond
-
-// for (let i = 1; i <= size; i++) {
-//   let padding = " ".repeat(size - i);
-//   console.log(padding + "*".repeat(2 * i - 1));
-// }
-
-// for (let i = size - 1; i >= 1; i--) {
-//   let padding = " ".repeat(size - i);
-//   console.log(padding + "*".repeat(2 * i - 1));
-// }
-
-let arr = ['areeba', 'habiba', 'aisha', 'afifa'];
-console.log(arr);
+for (let i=0; i<=10; i++){
+    for (let j=0; j<=10; j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+    console.log(); // Add a line space after every table ends
+}
