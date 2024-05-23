3bda7adc7d45e4fe431b35cc21e536c7d11ddcae
areebaqamar021 <areebaqamar021@gmail.com>
2024-05-23 15:48:27 +0500

    "Added solutions for 6 JavaScript problems: reversing an array, sorting an array, filtering out negative numbers, removing spaces from a string, checking divisibility by 10, counting vowels in a string, and calculating the factorial of a number."

--- javaScript-typescript/index.js
+++ javaScript-typescript/index.js
@@ -51,3 +51,73 @@ for (let i=0; i<numbers.length; i++){
 }
 console.log(sum);
 
+// Problem 6 : Create a function that reverses an array
+
+const num = [4, 3, 7, 9, 2];
+function reverseArray(arr){
+    arr.reverse();
+    return arr;
+}
+console.log(reverseArray(num));
+
+// Problem 7 : Sort an array from lowest to highest
+
+function sortArray(arr){
+    arr.sort();
+    return arr;
+}
+console.log(sortArray(num));
+
+// Problem 8 : Create a function that filters out negative numbers
+
+const negNum = [-3, -9, 6, 8, -5, -2];
+function filterNegative(arr){
+    let result = arr.filter(num => num >= 0);
+    return result;
+}
+console.log(filterNegative(negNum));
+
+// Problem 9 : Remove the spaces found in a string
+
+const str = 'Hello         World';
+function removeSpaces(str){
+    let result = str.replace(/\s/g, '');
+    return result;
+}
+console.log(removeSpaces(str));
+
+// Problem 10 :  Return a Boolean if a number is divisible by 10
+
+function divisibleByTen(num){
+    if(num % 10 === 0){
+        return true;
+    }
+    return false;
+}
+console.log(divisibleByTen(57));
+
+// Problem 11 : Return the number of vowels in a string
+
+const str1 = 'Hello World';
+function countVowels(str){
+    let count = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i=0; i<str.length; i++){
+        if(vowels.includes(str[i])){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countVowels(str1));
+
+// Problem 12 : Create a function that returns the factorial of a number
+
+function factorial(num){
+    let result = 1;
+    for (let i=1; i<=num; i++){
+        result *= i;
+    }
+    return result;
+}
+console.log(factorial(5));
